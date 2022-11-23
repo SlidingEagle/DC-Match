01473b8103f6547e141847c5220dba78ba2fdb7b
Yi Gu <guyi0629@163.com>
2022-11-23 20:00:54 +0800

    first commit

---
+++ src/others/logging.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from pathlib import Path
+import logging
+import sys
+
+logger = logging.getLogger()
+
+
+def init_logger(log_file=None, log_file_level=logging.NOTSET):
+    log_format = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_format)
+    logger.handlers = [console_handler]
+
+    if log_file and log_file != '':
+        # print(log_file)
+        # print(sys.path)
+        # sys.path.append(Path(log_file))
+
+
+        file_handler = logging.FileHandler(Path(log_file))
+        file_handler.setLevel(log_file_level)
+        file_handler.setFormatter(log_format)
+        logger.addHandler(file_handler)
+    print(file_handler)
+    return logger
